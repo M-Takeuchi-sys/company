de79a8ee94eb5651504c9dc2d466e11863b34dca
masao <>
2021-06-25 11:44:09 +0900

    更新機能を追加

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('employee_create', views.EmployeeCreate.as_view(), name='employee_create'),
     path('employee_detail/<int:pk>', views.EmployeeDetail.as_view(), name='employee_detail'),
+    path('employee_update/<int:pk>', views.EmployeeUpdate.as_view(), name='employee_update'),
 ]
